217d9546a7b12b73436b88d9271ccc4b5c7f0e8b
Bezbuldabyz <buldyrabyz@gmail.com>
2018-09-26 01:51:54 +0300

    simple import

--- script.py
+++ script.py
@@ -1,7 +1,10 @@
 # Первый сценарий на языке Python
 
 import sys
+import myfile
+
 print(sys.platform)
+print(myfile.title)
 print(2**100)
 x="Spam!"
 print(x*8)
@@ -9,3 +12,5 @@ print(x*8)
 #-- for example
 for  x in "spam":
     print(x)
+
+input()
