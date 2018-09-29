ac74bbe89308f107d0001a19aef8c224605708fa
bezbuldyrabyz <buldyrabyz@gmil.com>
2018-09-29 09:02:23 +0300

    временного отключения строк программного кода

---
+++ srt.py
@@ -0,0 +1,20 @@
+# Неформатированные строки подавляют экранирование
+
+path=r"C:\new\text.dat"
+print(path)
+
+# Тройные кавычки, многострочные блоки текста
+
+mantra="""Always look
+    on the bright
+    side of life."""
+print(mantra)
+
+
+x=1
+"""
+import os
+print(os.getcwd())
+"""
+y=2
+print(x)
