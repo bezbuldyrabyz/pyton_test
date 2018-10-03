dea30e8be606bf5b55299ccc43c11f24bb20a1dc
bezbuldyrabyz <buldyrabyz@gmil.com>
2018-10-03 17:08:05 +0300

    строки и базовые методы

--- srt.py
+++ srt.py
@@ -71,3 +71,38 @@ print(l)
 s="".join(l)
 print(s)
 print("SPAM".join(["eggs","sausage","ham","toast"]))
+
+
+# строки и базовые методы http://pythonz.net/references/named/str/
+
+line="     the knights who sy Ni!\n"
+line.capitalize() # Возвращает копию строки, делая первую букву заглавной
+
+'ß'.lower()  # 'ß'
+print('ß'.casefold())  # 'ss' - Возвращает копию строки в сложенном регистре.
+
+print("".center(3,"w")) # Позиционирует по центру указанную строку, дополняя её справа и слева до указанной длины указанным символом.
+print("1".center(2,"w"))
+print("1".center(4,"w")) # ... Символ добавляется к строке циклично сначала справа, затем слева.
+print("1".center(0,"w"))
+print("1".center(4))
+
+my_str = "podstroka is strok"
+print(my_str.count("stroka")) # 1 - Для строки возвращает количество непересекающихся вхождений в неё указанной подстроки.
+my_str.count("str")  # 2
+my_str.count('str', 0, -1)  # 2
+
+# print(getdefaultencoding())  # utf-8
+my_string = 'кот cat'
+print(type(my_string))  # str
+print(my_string.encode())
+print(my_string.encode('ascii', errors='ignore')) # b' cat'
+my_string.encode('ascii', errors='replace') # b'??? cat'
+print(my_string.encode('ascii', errors='xmlcharrefreplace')) # b'&#1082;&#1086;&#1090; cat'
+
+
+print(line)
+print(line.rstrip()) # удаляют пробельные символы в конце текстовой строки
+print(line.upper())
+print(line.isalpha()) # Вернёт True, если в строке хотя бы один символ и все символы строки являются буквами, иначе — False.
+print(line.endswith("Ni!\n")) #Возвращает флаг, указывающий на то, заканчивается ли строка указанным постфиксом.
